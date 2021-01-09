1fc66ad827c1ddf2ee298ecbadef8fb81916fcbe
Evgenij Reznik <evgenij.reznik@gmx.de>
2021-01-09 14:57:30 +0100

    Refactoring

---
+++ src/main/java/docxjavamapper/model/DJMNumberingProperties.java
@@ -0,0 +1,11 @@
+package docxjavamapper.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class DJMNumberingProperties {
+// TODO
+}
